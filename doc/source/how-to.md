360b00132039da99d88622cfb47f232441456bfe
Jonas Witthuhn <witthuhn@tropos.de>
2022-02-23 14:52:21 +0100

    tools to prepare ancillary data

--- doc/source/how-to.md
+++ doc/source/how-to.md
@@ -5,4 +5,29 @@ For functionality a set of ancillary data (see {ref}`sec-intro-ancillary`) is ne
    * correct measured irradiance due to alignment of the instrument (ship movement)
    * estimate trace-gas absorption and Rayleigh-scattering optical depths in order to calculate the aerosol optical depth
 
+To prepare the required ancillary data one can use `modules/ncutils.create_nc_dataset`.
+This module requires the output filename, the Data in a dictionary with appropriate variable naming according to the `cfconv.json` configuration file.
+In the shrad-pro root directory one can find `trosat_cfmeta_ins.json` and `trosat_cfmeta_met.json` as a blueprint for the INS and MET data, respectively.
+
+### Example usage:
+   ```python
+   import numpy as np
+   import modules.ncutils as ncu
+   # example for INS data
+   data = {
+        'roll': np.array(...),
+        'pitch': np.array(...),
+        'yaw': np.array(...),
+        'lat': np.array(...),
+        'lon': np.array(...),
+        'time': np.array(..., dtype=np.datetime64),
+          } 
+   fname = "test.nc"
+   cfjson = "<path>/shrad-pro/trosat_cfmeta_ins.json"
+   attrs = {"note": "this note is added additionally as global attribute"}
+   # store the data to shrad-pro compatible netCDF4 file
+   ncu.create_nc_dataset(fname, data, cfjson, attrs)
+   ```
+
+The output files should be named and stored according to the settings in `ConfigFile.ini`
 
---
+++ modules/ncutils.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+
+from trosat import cfconv as cf
+
+
+def create_cf_dataset(fname, data, cfjson, attrs={}):
+    """
+    Create cf-compliant netCDF file using trosat.cfconv
+    
+    Parameters
+    ----------
+    fname: string
+        Output netCDF filename
+    data: dict or xarray.Dataset
+        Data to store. Variable names have to match definitions in the cfjson file.
+    cfjson: string
+        Filename of the json configuration file required for trosat.cfconv
+    attrs: dict
+        Additional global attributes for output file. This will update the
+        cfjson["attributes"] dict.
+    Returns
+    -------
+    """
+    # read cfdict
+    cfdict = cf.read_cfjson(cfjson)
+
+    # update global attributes
+    cfdict["attributes"].update(attrs)
+
+    # set appropriate time variable units to "<time> since <data-start-time>"
+    timedata = np.array(data["time"]).astype("datetime64[ms]")
+    starttime = pd.to_datetime(timedata[0])
+    timeunits = f"milliseconds since {starttime:%Y-%m-%d %H:%M:%S}"
+    cfdict["variables"]["time"]["attributes"]["units"] = timeunits
+
+    # the scale_factor attributes of variables in the cfjson file define the
+    # precision of the stored data. E.g. variables with a scale_factor of 0.1
+    # will be accurate to one decimal. The following loop adds the "add_offset"
+    # attribute to offset the values to be always positive.
+    for var in cfdict["variables"].keys():
+        if var == "time":
+            continue
+        scalefactor = cfdict["variables"][var]["attributes"]["scale_factor"]
+        # maximum precision of stored integer values
+        prec = np.int(np.ceil(np.abs(np.log10(scalefactor))))
+        # calculate and set offset based on precision and values
+        addoffset = np.round(np.min(data[var]), prec)
+        cfdict["variables"][var]["attributes"]["add_offset"] = addoffset
+
+    # create the file
+    f = cf.create_file(fname, cfdict=cfdict)
+    f.set_auto_scale(True)
+
+    # fill the data
+    # apply offset and convert time manually to milliseconds from start-time
+    convtime = (timedata-timedata[0]).astype(int)*1e-6
+    f["time"][:] = convtime
+
+    for var in cfdict["variables"].keys():
+        if var == "time":
+            continue
+        f[var][:] = np.array(data[var])
+
+    # close and save the file
+    f.close()
+    return 0
